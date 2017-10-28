0db984c6940d89fd3ecbe4e86ae32ed3fc87efda
xiaoyaliu <308912742@qq.com>
2017-10-28 11:22:49 +0800

    first commit

---
+++ src/components/entry.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <mt-cell title="Toast"  is-link></mt-cell>
+    <mt-cell title="Indicator"  is-link></mt-cell>
+    <mt-cell title="Loadmore"  is-link></mt-cell>
+    <mt-cell title="Infinite scroll"  is-link></mt-cell>
+    <mt-cell title="Message box"  is-link></mt-cell>
+    <mt-cell title="Action sheet"  is-link></mt-cell>
+    <mt-cell title="Popup"  is-link></mt-cell>
+    <mt-cell title="Swipe"  is-link></mt-cell>
+    <mt-cell title="Lazy load"  is-link></mt-cell>
+    <mt-cell title="Randge"  is-link></mt-cell>
+    <mt-cell title="Progress"  is-link></mt-cell>
+    <mt-cell title="Picker"  is-link></mt-cell>
+    <mt-cell title="Datetime Picker"  is-link></mt-cell>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+
+  }
+</script>
